3eaf93b071bda60d14c770e9ba0e14dbf089e3e2
Sander Nordeide <nordeidesander@gmail.com>
2024-05-19 19:09:07 +0200

    added styling

--- components/TypingAnimation.jsx
+++ components/TypingAnimation.jsx
@@ -1,9 +1,9 @@
 const TypingAnimation = () => {
     return (
-        <div className="flex items-center space-x-2">
-            <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse"></div>
-            <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-75"></div>
-            <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-150"></div>
+        <div className="flex items-center space-x-2 bg-blue-400">
+            <div className="w-4 h-4 rounded-full bg-blue-400 bg-gradient-to-r from-blue-600 to-blue-800 animate-pulse"></div>
+            <div className="w-4 h-4 rounded-full bg-blue-400 bg-gradient-to-r from-blue-600 to-blue-800 animate-pulse delay-75"></div>
+            <div className="w-4 h-4 rounded-full bg-blue-400 bg-gradient-to-r from-blue-600 to-blue-800 animate-pulse delay-150"></div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -69,20 +69,18 @@ const Home = () => {
   }
 
   return (
-    <div className="container mx-auto max-w-[700px]">
-      <div className="flex flex-col h-screen bg-gray-900">
-        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">ChatGPT</h1>
+    <div className="container mx-auto">
+      <div className="flex flex-col h-screen bg-stone-50">
+        <h1 className="bg-gradient-to-r from-blue-400 to-green-700 text-transparent bg-clip-text text-center py-3 font-bold text-7xl">UP-ORAKEL</h1>
         <div className="flex-grow p-6">
           <div className="flex flex-col space-y-4">
             {
               chatLog.map((message, index) => (
                 <div key={index} className={`flex ${message.type === 'user' ? 'justify-end' : 'justify-start'
                   }`}>
-                  <div className={`${message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
+                  <div className={`${message.type === 'user' ? 'bg-green-600' : 'bg-blue-400'
                     } rounded-lg p-4 text-white max-w-sm`}>
                     {message.message}
-                    {index}
-
                   </div>
                 </div>
               ))
@@ -99,7 +97,7 @@ const Home = () => {
             {
               isLoading &&
               <div key={chatLog.length} className="flex justify-start">
-                <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
+                <div className=" bg-blue-400 rounded-lg p-4 text-black max-w-sm">
                   <TypingAnimation />
                 </div>
               </div>
@@ -107,9 +105,9 @@ const Home = () => {
           </div>
         </div>
         <form onSubmit={handleSubmit} className="flex-none p-6">
-          <div className="flex rounded-lg border border-gray-700 bg-gray-800">
-            <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
+          <div className="flex rounded-lg border border-gray-700  text-black bg-white" >
+            <input type="text" className="flex-grow px-4 py-2 bg-transparent text-black focus:outline-none" placeholder="Spør meg om et matprodukt..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <button type="submit" className="bg-stone-400 rounded-lg px-4 py-2 text-black focus:outline-none hover:bg-stone-300 transition-colors duration-300">Send</button>
           </div>
         </form>
       </div>
