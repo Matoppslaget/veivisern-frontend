5c5595e10eee0de49c25872493d5886c7316ee24
Sander Nordeide <nordeidesander@gmail.com>
2024-08-27 22:09:58 +0200

    start from scratch

--- components/chat_components/ProductSceleton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { KassalappProduct } from '../../types/Kassalapp';
-
-
-const ProductSceleton = ({ product }: { product: KassalappProduct }) => {
-  return 
-};
-
-export default ProductSceleton;
