d6b97d8d04d830cdbdeba46894af36efa7b2a230
Sander Nordeide <nordeidesander@gmail.com>
2024-09-01 11:51:02 +0200

    remove comment

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
         <div>
             <div className="p-4 px-10 mx-auto max-w-4xl">
                 <div className="p-2 px-4 bg-white rounded-xl shadow-sm flex justify-between space-x-4 border">
-                    {/* <input placeholder="Søk etter produkt..." type="text" className="focus:text-black focus:ring-red-500 pt-1 text-gray-400" ></input> */}
                     <input placeholder="Søk etter produkt..." type="text" className="w-full rounded-md py-1.5 pl-7 pr-20 text-gray-900 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset focus:ring-1 focus:ring-green-700 focus:ring-opacity-50 focus:outline-none sm:text-sm sm:leading-6" ></input>
-
                     <MagnifyingGlassIcon className=" text-gray-500 w-10 h-10 justify-end hover:cursor-pointer hover:text-black"></MagnifyingGlassIcon>
                 </div>
             </div>
