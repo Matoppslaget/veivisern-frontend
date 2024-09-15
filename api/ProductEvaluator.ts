c5a38ba8474e17c87c79e59a39b9a28fc5394250
Sander Nordeide <nordeidesander@gmail.com>
2024-09-15 15:16:45 +0200

    fix build

--- api/ProductEvaluator.ts
+++ api/ProductEvaluator.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { EvaluatedProduct, EvaluatedProductResponse, KassalappProduct } from '@/components/KassalappResponse';
 
 const env = process.env.NEXT_PUBLIC_ENVIRONMENT;
-const ProductEvaluationEndpoint = env === 'prod' ? 'https://brainy-oneida-sanders-consulting-4fc76d82.koyeb.app/evaluate_product' : 'http://localhost:8000/evaluate_product';  // Point to the local FastAPI endpoint
+const ProductEvaluationEndpoint = env === 'prod' ? 'https://brainy-oneida-sanders-consulting-4fc76d82.koyeb.app/evaluate_product' : 'http://localhost:8000/evaluate_product';
 
 export const fetchProductEvaluation = async (product: KassalappProduct): Promise<EvaluatedProduct> => {
+    console.log(env)
+    console.log(ProductEvaluationEndpoint)
     try {
         const response: EvaluatedProductResponse = await axios.post(ProductEvaluationEndpoint, product);
         return response.data;
