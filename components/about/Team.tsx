194c71bb830c6cded07f8aeb5ec056578eaf9a52
Francesca Lumor <francesca.lumor@airthings.com>
2024-10-12 12:55:52 +0200

    Add Francesca profile picture

--- components/about/Team.tsx
+++ components/about/Team.tsx
@@ -11,7 +11,7 @@ export default function Team() {
             <div className="p-4 grid grid-cols-3">
                 <Image className="p-4 justify-self-end" src="/Anders profil.png" alt="Anders" width="520" height="520"></Image>
                 <Image className="p-4 justify-self-start" src="/Sander profil.png" alt="Sander" width="520" height="520"></Image>
-                <Image className="p-4 justify-self-start" src="" alt="Francesca" width="520" height="520"></Image>
+                <Image className="p-4 justify-self-start" src="/Francesca profil.png" alt="Francesca" width="520" height="520"></Image>
             </div >
             <div className="p-4">
                 <p className="text-md p-1 text-center">Teamet som har laget Matoppslaget er Anders, Sander og Francesca. </p>
