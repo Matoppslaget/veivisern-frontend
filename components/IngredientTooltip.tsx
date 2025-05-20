0623f2cdfc3d0dc7ffb1751f89263538daccc8c4
Sander Nordeide <nordeidesander@gmail.com>
2025-05-20 19:18:34 +0200

    dismiss tooltip

--- components/IngredientTooltip.tsx
+++ components/IngredientTooltip.tsx
@@ -7,29 +7,29 @@ export default function IngredientTooltip() {
     'Denne ingrediensen faller under kategori 4 av Nova-klassifiseringen';
 
   return (
-
-      <Tooltip
-        className="border border-blue-gray-50 bg-white px-4 py-3 shadow-xl shadow-black/10 !z-[9999]"
-        content={
-          <div className="w-80">
-            <Typography color="blue-gray" className="text-lg font-bold">
-              {titleText}
-            </Typography>
-            <Typography
-              variant="small"
-              color="blue-gray"
-              className="text-lg opacity-80"
-            >
-              {detailText}
-            </Typography>
-          </div>
-        }
-        animate={{
-          mount: { scale: 1, y: 0 },
-          unmount: { scale: 0, y: 25 },
-        }}
-      >
-        <QuestionMarkCircleIcon className="hover:cursor-pointer pl-2 w-[34px] h-[28px]"></QuestionMarkCircleIcon>
-      </Tooltip>
+    <Tooltip
+      dismiss={{ ancestorScroll: true }}
+      className="border border-blue-gray-50 bg-white px-4 py-3 shadow-xl shadow-black/10 !z-[9999]"
+      content={
+        <div className="w-80">
+          <Typography color="blue-gray" className="text-lg font-bold">
+            {titleText}
+          </Typography>
+          <Typography
+            variant="small"
+            color="blue-gray"
+            className="text-lg opacity-80"
+          >
+            {detailText}
+          </Typography>
+        </div>
+      }
+      animate={{
+        mount: { scale: 1, y: 0 },
+        unmount: { scale: 0, y: 25 },
+      }}
+    >
+      <QuestionMarkCircleIcon className="hover:cursor-pointer pl-2 w-[34px] h-[28px]"></QuestionMarkCircleIcon>
+    </Tooltip>
   );
 }
